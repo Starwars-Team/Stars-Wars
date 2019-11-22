286faed50a099f00cc00a189e116335b9aceb4bf
amirullah23 <amirullah.kasang@gmail.com>
2019-11-22 13:26:49 +0700

    add compenents

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios')
+
 function App() {
+
+
+
+  axios.get('https://swapi.co/api/vehicles')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+
+
   return (
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
