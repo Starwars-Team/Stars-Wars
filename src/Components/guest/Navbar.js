a4e94a6c80025ced6da092bbbf099da1224c0068
Hafiz Yanuar Ibrahim <hafizyanuar95@gmail.com>
2019-11-24 10:00:25 +0700

    edit css & guest display

---
+++ src/Components/guest/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <div
+            style={{ flexGrow: 1}}>
+                <AppBar 
+                position="static"
+                style={{
+                    color : 'white', 
+                    backgroundColor:'black'
+                }}
+                >
+                    <Toolbar>
+                        <Typography
+                        className="navbarMember"
+                        variant="h6" 
+                        style={{ 
+                            flexGrow: 1,
+                        }}
+                        >
+                        Star Wars
+                        </Typography>
+                        <Button 
+                        className="navbarMember"
+                        color="inherit"
+                        >
+                        Register
+                        </Button>
+                        ||
+                        <Button 
+                        className="navbarMember"
+                        color="inherit"
+                        >
+                        Login
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
